bc3714d1c33facb5799f3fb4e42f9fa0f97bcc16
Mactavish <maczhanchao@yahoo.com>
2018-05-23 17:41:43 +0800

    feat:

--- packages/divide/index.js
+++ packages/divide/index.js
@@ -1,3 +1,3 @@
-export function sum(a, b) {
-  return a - b
+export function divide (a, b) {
+  return a / b
 }
--- packages/sum/index.js
+++ packages/sum/index.js
@@ -1,3 +1,3 @@
-export function divide (a, b) {
-  return a - b
+export function sum (a, b) {
+  return a + b
 }
